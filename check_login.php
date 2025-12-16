1d1bb76f6f78a56897bfa3a8d8f344a950489cb9
Alhaking1 <mjyblwan0@gmail.com>
2025-12-16 20:32:36 +0300

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -1,1 +1,19 @@
 
+<?php
+session_start();
+
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header('Location: login.html');
+    exit();
+}
+
+// التحقق من مدة الجلسة (4 ساعات)
+if (isset($_SESSION['login_time'])) {
+    $session_timeout = 4 * 60 * 60; // 4 ساعات بالثواني
+    if (time() - $_SESSION['login_time'] > $session_timeout) {
+        session_destroy();
+        header('Location: login.html?expired=1');
+        exit();
+    }
+}
+?>
